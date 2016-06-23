b33dfa0c9aa31c96dc088ce81b5eb397ec8ec0cc
shepherd-t <shepherd-t@users.noreply.github.com>
2016-06-23 00:12:08 -0500

    Add first Ruby program
    covered...
    Installing Ruby
    IRB
    Writing a Ruby Program
    Ruby Gems (Sinatra)

---
+++ hello.rb
@@ -0,0 +1,38 @@
+# This is my first Ruby program
+
+puts "Hello IRB!"
+puts 1 + 1
+puts "Hello IRB!".reverse
+puts "2 is an even number" if 2.even?
+[2,8,3,6].sort
+3.times do
+puts "Ruby!"
+end
+exit
+
+
+# gem install sinatra
+
+=begin
+WARNING:  Installing to ~/.gem since /Library/Ruby/Gems/1.8 and
+	  /usr/bin aren't both writable.
+WARNING:  You don't have /Users/.../.gem/ruby/1.8/bin in your PATH,
+	  gem executables will not run.
+Successfully installed rack-1.6.4
+Successfully installed tilt-2.0.5
+Successfully installed rack-protection-1.5.3
+Successfully installed sinatra-1.4.7
+4 gems installed
+Installing ri documentation for rack-1.6.4...
+Installing ri documentation for tilt-2.0.5...
+Installing ri documentation for rack-protection-1.5.3...
+Installing ri documentation for sinatra-1.4.7...
+
+unrecognized option `--encoding=UTF-8'
+
+For help on options, try 'rdoc --help'
+
+=end
+
+
+# update Ruby
